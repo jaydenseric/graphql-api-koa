f7dadd629882be2b97c1f757b11c7e1cda4c51ff
Jayden Seric <me@jaydenseric.com>
2018-05-15 13:50:59 +1000

    Fresh project setup without AVA.

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,7 @@ module.exports = {
       '@babel/env',
       {
         targets: { node: node.substring(2) }, // Strip `>=`
-        modules: process.env.ESM ? false : 'commonjs',
+        modules: process.env.BABEL_ESM ? false : 'commonjs',
         loose: true
       }
     ]
