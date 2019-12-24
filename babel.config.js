a74a5b24184e9cecdc0b497fedcb58395e80c4a8
Jayden Seric <me@jaydenseric.com>
2019-12-24 13:22:57 +1100

    Temporarily disable code coverage, workaround tap issues.

--- babel.config.js
+++ babel.config.js
@@ -4,28 +4,8 @@ const {
   engines: { node }
 } = require('./package.json')
 
-/**
- * A Babel plugin that adds `istanbul ignore next` comments before Babel
- * `_interopRequireDefault` function declarations, to allow 100% code coverage.
- * @kind function
- * @name babelPluginIstanbulIgnoreBabelInterop
- * @returns {Function} The Babel plugin.
- * @ignore
- */
-const babelPluginIstanbulIgnoreBabelInterop = () => ({
-  visitor: {
-    FunctionDeclaration(path) {
-      if (path.node.id.name === '_interopRequireDefault')
-        path.addComment('leading', ' istanbul ignore next', true)
-    }
-  }
-})
-
 module.exports = {
-  shouldPrintComment: comment =>
-    // Preserve Istanbul ignore comments.
-    /@license|@preserve|istanbul ignore/.test(comment),
-  plugins: [babelPluginIstanbulIgnoreBabelInterop],
+  comments: false,
   presets: [
     [
       '@babel/env',
