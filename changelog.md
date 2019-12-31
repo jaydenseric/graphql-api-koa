d9b379f67db18ab8285c3351056be07117f0581a
Jayden Seric <me@jaydenseric.com>
2019-12-31 17:42:03 +1100

    Simplify the test index file.

--- changelog.md
+++ changelog.md
@@ -5,6 +5,7 @@
 ### Patch
 
 - Added a new [`hard-rejection`](https://npm.im/hard-rejection) dev dependency to ensure unhandled rejections in tests exit the process with an error.
+- Simplified `test/index.js`.
 
 ## 4.0.0
 
--- test/index.js
+++ test/index.js
@@ -1,20 +1,14 @@
 'use strict'
 
 const { TestDirector } = require('test-director')
-const checkGraphQLSchema = require('./checkGraphQLSchema.test')
-const checkGraphQLValidationRules = require('./checkGraphQLValidationRules.test')
-const checkOptions = require('./checkOptions.test')
-const errorHandler = require('./errorHandler.test')
-const execute = require('./execute.test')
-const isEnumerableObject = require('./isEnumerableObject.test')
 
 const tests = new TestDirector()
 
-isEnumerableObject(tests)
-checkOptions(tests)
-checkGraphQLValidationRules(tests)
-checkGraphQLSchema(tests)
-errorHandler(tests)
-execute(tests)
+require('./checkGraphQLSchema.test')(tests)
+require('./checkGraphQLValidationRules.test')(tests)
+require('./checkOptions.test')(tests)
+require('./errorHandler.test')(tests)
+require('./execute.test')(tests)
+require('./isEnumerableObject.test')(tests)
 
 tests.run()
